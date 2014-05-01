7e70fdf6aca283c5669bd93912db411553b48cdf
Rachel Donahue <donahrm@gmail.com>
2014-05-01 16:39:19 -0400

    removed if __FILE__ == #0

--- lib/nal_mods/classes/identifier.rb
+++ lib/nal_mods/classes/identifier.rb
@@ -15,15 +15,3 @@ module NalMods
 		end
 	end
 end
-
-if __FILE__ == $0
-	xmldoc = (Document.new File.new "test.xml")
- 	
- 	xmldoc.elements.each("mods/identifier") do |e|
-		id = ModsIdentifier.new
-		id.type = e.attributes['type'] unless e.attributes['type'].nil?
-		id.value = e.text unless e.nil?
-
-		id.print_vals
-	end
-end
