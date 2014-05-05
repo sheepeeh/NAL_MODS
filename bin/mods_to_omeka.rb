1b4408d1bd61980beae9b9087c29e28ba47be7b4
Rachel Donahue <donahrm@gmail.com>
2014-05-05 17:18:33 -0400

    initial commit

---
+++ bin/mods_to_omeka.rb
@@ -0,0 +1,266 @@
+require 'csv'
+require 'rexml/document'
+require 'benchmark'
+
+# Options
+require_relative '../lib/nal_mods/config'
+
+# Mixins
+require_relative '../lib/nal_mods/modules/retrievable'
+require_relative '../lib/nal_mods/modules/printable'
+require_relative '../lib/nal_mods/modules/walkable'
+
+# Classes
+require_relative '../lib/nal_mods/classes/abstract'
+require_relative '../lib/nal_mods/classes/access'
+require_relative '../lib/nal_mods/classes/genre'
+require_relative '../lib/nal_mods/classes/identifier'
+require_relative '../lib/nal_mods/classes/language'
+require_relative '../lib/nal_mods/classes/location'
+require_relative '../lib/nal_mods/classes/name'
+require_relative '../lib/nal_mods/classes/note'
+require_relative '../lib/nal_mods/classes/origin'
+require_relative '../lib/nal_mods/classes/physdesc'
+require_relative '../lib/nal_mods/classes/related'
+require_relative '../lib/nal_mods/classes/resourcetype'
+require_relative '../lib/nal_mods/classes/subject'
+require_relative '../lib/nal_mods/classes/title'
+require_relative '../lib/nal_mods/classes/toc'
+
+include NalMods::Printable
+include NalMods::Retrievable
+include NalMods::Walkable
+include REXML
+
+module NalMods
+
+	def pjoin(arr)
+		arr.compact!
+		arr = arr.join("^")
+	end
+
+	def build_csv(target_dir)
+		btime = Benchmark.realtime {
+		Dir.mkdir("#{@script_options[:normalized_dir]}") unless Dir.exists?("#{@script_options[:normalized_dir]}")
+		f = File.open("#{@script_options[:csv_destination]}","a")
+
+		# Print header
+		f.puts "original_filename|new_filename|identifier|title|name|origin|place|date_copyright|startdate_copyright|enddate_copyright|date_created|startdate_created|enddate_created|date_issued|startdate_issued|enddate_issued|genre|language|pdesc_form|pdesc_extent|shelf_loc|table_of_contents|subj_topic|subj_time|subj_genre|subj_geo|subj_hgeo|subj_name|subj_title"
+
+		Dir.glob("#{target_dir}/*.xml").each do |xml_file|
+
+			stime = Time.now
+			puts "#{stime}> Converting #{xml_file}"
+
+			# Setup files
+			xmldoc = (Document.new File.new "#{xml_file}")
+			original_filename = File.basename(xml_file)
+			fseed = File.basename(xml_file,".xml")
+			new_filename = "#{fseed}#{@script_options[:normalized_suffix]}"
+
+
+			# Create element arrays/variables
+			@identifiers = []
+			@titles = []
+			@names = []
+			@origins = []
+			@date_copyright = []
+			@startdate_copyright = nil
+			@enddate_copyright = nil
+			@date_created = []
+			@startdate_created = nil
+			@enddate_created = nil
+			@date_issued = []
+			@startdate_issued = nil
+			@enddate_issued = nil
+			@genres = []
+			@languages = []
+			@pdescs = []
+			@pdesc_extent = nil
+			@shelf_loc = []
+			@table_of_contents = []
+			@subj_topics = []
+			@subj_genres = []
+			@subj_times = []
+			@subj_geos = []
+			@subj_hgeos = []
+			@subj_names = []
+			@subj_titles = []
+
+			# RETRIEVE MODS DATA
+
+			## ID
+			xmldoc.elements.each("mods/identifier") do |e|
+				@identifiers << e.text			
+			end
+
+			
+
+			## TITLE
+			
+		 	xmldoc.elements.each("mods/titleInfo") do |e|
+				title = ModsTitle.new
+				title.get_title(e)
+				title.vals2omeka(@titles)
+			end
+
+			
+
+			## NAME
+			xmldoc.elements.each("mods/name") do |e|
+				name = ModsName.new
+				name.get_name(e)
+				name.vals2omeka(@names)
+			end
+
+
+			
+			## GENRE
+			xmldoc.elements.each("mods/genre") do |e|
+				@genres << e.text
+			end
+
+
+			## ORIGIN INFO
+			xmldoc.elements.each("mods/originInfo") do |e|
+				origin = ModsOrigin.new
+				origin.get_publisher(e)
+				origin.get_place(e)
+				origin.get_issuance(e)
+				origin.vals2omeka(@origins)
+
+				copyright = ModsOrigin::Copyright.new
+				copyright.get_date(e)
+				copyright.vals2omeka(@date_copyright)
+
+				created = ModsOrigin::Created.new
+				created.get_date(e)
+				created.vals2omeka(@date_created)
+
+				issued = ModsOrigin::Issued.new
+				issued.get_date(e)
+				issued.vals2omeka(@date_issued)
+			end
+
+
+
+
+
+			## LANGUAGE
+			xmldoc.elements.each("mods/language") do |e|
+				lang = ModsLang.new
+				lang.get_ltext(e)
+				lang.get_lcode(e)
+				lang.vals2omeka(@languages)			
+			end
+
+
+			## PHYSICAL DESCRIPTION
+			xmldoc.elements.each("mods/physicalDescription") do |e|
+				pdesc = ModsPhysDesc.new
+				pdesc.get_pdesc(e)
+				pdesc.vals2omeka(@pdescs)
+			end
+
+			
+
+			## SUBJECTS
+			xmldoc.elements.each("mods/subject/topic") do |e|
+				subj_topic = ModsSubject.new
+				subj_topic.get_topic(e)
+				subj_topic.vals2omeka(@subj_topics)
+			end
+
+
+
+			xmldoc.elements.each("mods/subject") do |e|
+				subj_genre = ModsSubject.new
+				subj_genre.get_genre(e)
+				subj_genre.vals2omeka(@subj_genres)
+			end
+
+
+
+			xmldoc.elements.each("mods/subject") do |e|
+				geo = ModsSubject.new
+				geo.get_geo(e)
+				geo.vals2omeka(@subj_geos)
+			end
+
+			xmldoc.elements.each("mods/subject") do |e|
+				time = ModsSubject.new
+				time.get_time(e)
+				time.vals2omeka(@subj_times)
+			end
+
+
+
+			xmldoc.elements.each("mods/subject/hierarchicalGeographic") do |h|
+				hgeo = ModsSubject::HGeo.new
+				hgeo.get_hgeo(h)
+				hgeo.vals2omeka(@subj_hgeos)
+			end
+
+
+
+			xmldoc.elements.each("mods/subject/name") do |e|
+				name = ModsName.new
+				name.get_name(e)
+				name.vals2omeka(@subj_names)
+			end
+
+
+			xmldoc.elements.each("mods/subject/titleInfo") do |e|
+				title = ModsTitle.new
+				title.get_title(e)
+				title.vals2omeka(@subj_titles)
+			end
+
+
+
+			## TABLE OF CONTENTS
+			xmldoc.elements.each("mods/tableOfContents") do |e|
+				toc = ModsToc.new
+				toc.get_toc(e)
+				toc.vals2omeka(@table_of_contents)
+			end
+
+
+
+			## PHYSICAL LOCATION
+			xmldoc.elements.each("mods/location") do |e|
+				location = ModsLocation.new
+				location.get_loc(e)
+				location.vals2omeka(@shelf_loc)	
+			end
+
+			element_arrays = [@identifiers, @titles, @names, @origins, @date_copyright, @date_created, @date_issued, @genres, @languages, @pdescs, @shelf_loc, @table_of_contents, @subj_topics, @subj_genres, @subj_times, @subj_geos, @subj_hgeos, @subj_names, @subj_titles]
+			element_names = ["identifiers", "titles", "names", "origins", "date_copyright", "date_created", "date_issued", "genres", "languages", "pdescs", "shelf_loc", "table_of_contents", "subj_topics", "subj_genres", "subj_times", "subj_geos", "subj_hgeos", "subj_names", "subj_titles"]
+
+			element_arrays.map! do |e_arr|
+				pjoin(e_arr) unless e_arr.nil?
+			end
+
+			element_hash = {}
+			element_names.each do |name|
+				i = element_names.index(name)
+				k = name
+				v = element_arrays[i]
+				element_hash.store(k,v)
+			end	
+
+			element_hash.each do |key, value|
+				instance_variable_set("@#{key}",value)
+			end		
+
+			f.puts "#{original_filename}|#{new_filename}|#{@identifiers}|#{@titles}|#{@names}|#{@origins}||#{@date_copyright}|#{@startdate_copyright}|#{@enddate_copyright}|#{@date_created}|#{@startdate_created}|#{@enddate_created}|#{@date_issued}|#{@startdate_issued}|#{@enddate_issued}|#{@genres}|#{@languages}|#{@pdescs}||#{@shelf_loc}|#{@table_of_contents}|#{@subj_topics}|#{@subj_times}|#{@subj_genres}|#{@subj_geos}|#{@subj_hgeos}|#{@subj_names}|#{@subj_titles}"
+		end
+		f.close
+						}
+		etime = Time.now
+		puts "#{etime}> Time elapsed: #{btime}"
+	end
+
+end
+
+include NalMods
--- lib/nal_mods/modules/printable.rb
+++ lib/nal_mods/modules/printable.rb
@@ -8,7 +8,7 @@ module NalMods
 		def xmldoc
 			@xmldoc ||= 0 
 		end
-
+# Print values to stdout
 		def print_vals
 			objParts = {}
 
@@ -23,10 +23,9 @@ module NalMods
 				puts "#{key.to_s.capitalize}: #{value.to_s}" unless value.nil? || ["element","rvalue"].include?(key)
 			end
 		end
-
+# Send values to CSV for Open Refine
 		def vals2csv(arr)
 			objParts = {}
-			ct = []
 			mem = []
 
 			instance_variables.each do |v|
@@ -34,17 +33,9 @@ module NalMods
 				remove_instance_variable(v) if [nil,""].include?(val)
 				v = v.to_s.gsub!("@","")
 				objParts.store(v,val) unless [nil,""].include?(val)
-				ct << v unless val.nil?
 			end
 			
 			objParts.each do |key, value|
-			# 	# case ct.count
-			# 	# when 1
-			# 	# mem = "#{key.to_s.upcase} => #{value.to_s}" unless value.nil? || ["element","rvalue"].include?(key)
-			# 	# else
-			# 	# mem = "#{key.to_s.upcase} => #{value.to_s}%" unless value.nil? || ["element","rvalue"].include?(key)
-			# 	# end
-			# 	# arr << mem
 				memm =  "@#{key.to_s.upcase}&@ #{value.to_s}" unless value.nil? || ["element","rvalue"].include?(key)
 				mem << memm
 			end
@@ -52,5 +43,23 @@ module NalMods
 			mem = mem.join("%%")
 			arr << mem unless mem.empty?
 		end
+# Send values to CSV for Omeka CSV Import
+		def vals2omeka(var)
+			objParts = {}
+			mem = []
+
+			instance_variables.each do |v|
+				val = instance_variable_get(v)
+				remove_instance_variable(v) if [nil,""].include?(val)
+				v = v.to_s.gsub!("@","")
+				objParts.store(v,val) unless [nil,""].include?(val)
+			end
+			
+			objParts.each do |key, value|
+				mem =  "#{value.to_s}" unless ["",nil].include?(value)
+				arr << mem unless mem.empty?
+			end
+		end
+
 	end
 end
